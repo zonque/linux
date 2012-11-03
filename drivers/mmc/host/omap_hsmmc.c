e3a5463b75e68c6be19f550e8f29bb358e4b5f1f
Daniel Mack <zonque@gmail.com>
2012-11-03 09:50:39 +0100

    omap_hsmmc clock hack

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -42,6 +42,33 @@
 #include <linux/pm_runtime.h>
 #include <linux/platform_data/mmc-omap.h>
 
+
+
+#include <linux/clk.h>
+#include <linux/gpio.h>
+static void apply_clk_hack(struct device *dev)
+{
+	struct clk *ck_32;
+	int gpio = 64;
+
+	printk("%s()\n", __func__);
+	devm_gpio_request_one(dev, gpio, GPIOF_OUT_INIT_LOW, "wifi-pdn");
+	gpio_set_value(gpio, 0);
+	udelay(1000);
+
+	ck_32 = clk_get(NULL, "clk_32768_ck");
+        if (IS_ERR(ck_32)) {
+		printk(KERN_ERR "%s() :: Cannot clk_get()!\n", __func__);
+		return;
+        }
+
+	clk_enable(ck_32);
+	udelay(1000);
+	gpio_set_value(gpio, 1);
+}
+
+
+
 /* OMAP HSMMC Host Controller Registers */
 #define OMAP_HSMMC_SYSSTATUS	0x0014
 #define OMAP_HSMMC_CON		0x002C
@@ -1853,6 +1880,8 @@ static int omap_hsmmc_probe(struct platform_device *pdev)
 	struct pinctrl *pinctrl;
 	const struct omap_mmc_of_data *data;
 	void __iomem *base;
+
+	apply_clk_hack(&pdev->dev);
 
 	match = of_match_device(of_match_ptr(omap_mmc_of_match), &pdev->dev);
 	if (match) {
