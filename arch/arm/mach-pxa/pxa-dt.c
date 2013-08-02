36d85d1903f4470c61d4e34c89db7425f1bbe377
Daniel Mack <zonque@gmail.com>
2013-08-02 11:04:41 +0200

    PXA-DT: MFP HACK

--- arch/arm/mach-pxa/pxa-dt.c
+++ arch/arm/mach-pxa/pxa-dt.c
@@ -16,8 +16,63 @@
 #include <asm/mach/time.h>
 #include <mach/irqs.h>
 #include <mach/pxa3xx.h>
+#include <plat/mfp.h>
+#include <mach/mfp-pxa300.h>
 
 #include "generic.h"
+
+
+static mfp_cfg_t raumfeld_connector_pin_config[] __initdata = {
+	/* UART1 */
+	GPIO77_UART1_RXD,
+	GPIO78_UART1_TXD,
+	GPIO79_UART1_CTS,
+	GPIO81_UART1_DSR,
+	GPIO83_UART1_DTR,
+	GPIO84_UART1_RTS,
+
+	/* UART3 */
+	GPIO110_UART3_RXD,
+
+	/* USB Host */
+	GPIO0_2_USBH_PEN,
+	GPIO1_2_USBH_PWR,
+
+	/* I2C */
+	GPIO21_I2C_SCL | MFP_LPM_FLOAT | MFP_PULL_FLOAT,
+	GPIO22_I2C_SDA | MFP_LPM_FLOAT | MFP_PULL_FLOAT,
+
+	/* SPI */
+	GPIO34_GPIO,	/* SPDIF_CS */
+	GPIO96_GPIO,	/* MCLK_CS */
+	GPIO125_GPIO,	/* ACCEL_CS */
+
+	/* MMC */
+	GPIO3_MMC1_DAT0,
+	GPIO4_MMC1_DAT1,
+	GPIO5_MMC1_DAT2,
+	GPIO6_MMC1_DAT3,
+	GPIO7_MMC1_CLK,
+	GPIO8_MMC1_CMD,
+
+	/* Ethernet */
+	GPIO1_nCS2,			/* CS */
+	GPIO40_GPIO | MFP_PULL_HIGH,	/* IRQ */
+
+	/* SSP for I2S */
+	GPIO85_SSP1_SCLK,
+	GPIO89_SSP1_EXTCLK,
+	GPIO86_SSP1_FRM,
+	GPIO87_SSP1_TXD,
+	GPIO88_SSP1_RXD,
+	GPIO90_SSP1_SYSCLK,
+
+	/* SSP2 for S/PDIF */
+	GPIO25_SSP2_SCLK,
+	GPIO26_SSP2_FRM,
+	GPIO27_SSP2_TXD,
+	GPIO29_SSP2_EXTCLK,
+};
 
 #ifdef CONFIG_PXA3xx
 extern void __init pxa3xx_dt_init_irq(void);
@@ -46,6 +101,7 @@ static void __init pxa3xx_dt_init(void)
 {
 	of_platform_populate(NULL, of_default_bus_match_table,
 			     pxa3xx_auxdata_lookup, NULL);
+	pxa3xx_mfp_config(ARRAY_AND_SIZE(raumfeld_connector_pin_config));
 }
 
 static const char *pxa3xx_dt_board_compat[] __initdata = {
