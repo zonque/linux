665e5d734e62f16c5a2cae3d6c7810b356e770f4
Vaibhav Bedia <vaibhav.bedia@ti.com>
2013-08-06 12:49:36 -0500

    ARM: OMAP2+: AM33XX: Hookup AM33XX PM code into OMAP builds
    With all the requisite changes in place we can now
    enable the basic PM support for AM335x. This patch
    updates the various OMAP files to enable suspend-resume
    on AM335x.
    
    Signed-off-by: Vaibhav Bedia <vaibhav.bedia@ti.com>
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -311,7 +311,8 @@ int __init omap2_common_pm_late_init(void)
 	omap_init_cpufreq();
 
 #ifdef CONFIG_SUSPEND
-	suspend_set_ops(&omap_pm_ops);
+	if (!soc_is_am33xx())
+		suspend_set_ops(&omap_pm_ops);
 #endif
 
 	return 0;
